1858a29eaeac2ee3a9d9c52b0b2dae2db6572c2a
brianq25000 <122327043+brianq25000@users.noreply.github.com>
2025-10-01 20:24:07 -0500

    Update ExponentialInsulinModelPreset.swift

--- LoopKit/Insulin/ExponentialInsulinModelPreset.swift
+++ LoopKit/Insulin/ExponentialInsulinModelPreset.swift
@@ -21,11 +21,11 @@ extension ExponentialInsulinModelPreset {
     public var actionDuration: TimeInterval {
         switch self {
         case .rapidActingAdult:
-            return .minutes(240)
+            return .minutes(300)
         case .rapidActingChild:
             return .minutes(360)
         case .fiasp:
-            return .minutes(240)
+            return .minutes(300)
         case .lyumjev:
             return .minutes(360)
         case .afrezza:
